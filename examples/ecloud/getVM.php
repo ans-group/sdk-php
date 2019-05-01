bdf7a2e8168f3be3519e96cc19f6f6e21d1a3c36
Gavin Taylor <gavin.taylor@ukfast.co.uk>
2019-05-01 15:40:41 +0100

    remove debugging

--- examples/ecloud/getVM.php
+++ examples/ecloud/getVM.php
@@ -2,7 +2,7 @@
 
 // example of listing eCloud Virtual Machines
 //
-// php examples/ecloud/getVM.php 97254
+// php examples/ecloud/getVM.php 12345
 
 require_once dirname(__FILE__) . '/../../vendor/autoload.php';
 
@@ -14,4 +14,3 @@ $id = $argv[1];
 $virtualMachine = $ecloud->getVirtualMachine($id);
 
 print_r($virtualMachine);
-//var_dump($virtualMachine);
