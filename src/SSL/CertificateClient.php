4fe9f8553c47cb8b533e440bc1d32af0c36cba51
Gavin Taylor <gavin@gavtaylor.uk>
2019-05-19 20:45:30 +0100

    split ssl client into entity client

---
+++ src/SSL/CertificateClient.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace UKFast\SSL;
+
+use UKFast\Page;
+use UKFast\SSL\Entities\Certificate;
+use UKFast\SSL\Entities\CertificatePEM;
+
+class CertificateClient extends Client
+{
+    /**
+     * Gets a paginated response of all certificates
+     *
+     * @param int $page
+     * @param int $perPage
+     * @param array $filters
+     * @return Page
+     */
+    public function getAll($page = 1, $perPage = 15, $filters = [])
+    {
+        $page = $this->paginatedRequest('v1/certificates', $page, $perPage, $filters);
+        $page->serializeWith(function ($item) {
+            return new Certificate($item);
+        });
+
+        return $page;
+    }
+
+    /**
+     * Gets an individual Certificate
+     *
+     * @param int $id
+     * @return Certificate
+     */
+    public function getById($id)
+    {
+        $response = $this->request("GET", "v1/certificates/$id");
+        $body = $this->decodeJson($response->getBody()->getContents());
+        return new Certificate($body->data);
+    }
+
+    /**
+     * Get certificate private key
+     *
+     * @param int $id
+     * @return string
+     */
+    public function getCertificatePrivateKey($id)
+    {
+        $response = $this->request("GET", "v1/certificates/$id/private-key");
+        $body = $this->decodeJson($response->getBody()->getContents());
+
+        return $body->data->key;
+    }
+
+
+    /**
+     * Download certificate in PEM format
+     *
+     * @param int $id
+     * @return CertificatePEM
+     */
+    public function getCertificatePEM($id)
+    {
+        $response = $this->request("GET", "v1/certificates/$id/download");
+        $body = $this->decodeJson($response->getBody()->getContents());
+
+        return new CertificatePEM($body->data);
+    }
+}
--- src/SSL/CertificatePEM.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace UKFast\SSL;
-
-class CertificatePEM
-{
-    public $server;
-    public $intermediate;
-}
