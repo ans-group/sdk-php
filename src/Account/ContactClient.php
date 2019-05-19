88d91fd9cc77d7a165a27e5d80f3b7a6bf71be4f
Gavin Taylor <gavin@gavtaylor.uk>
2019-05-19 21:25:48 +0100

    split account client into entity clients

---
+++ src/Account/ContactClient.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace UKFast\Account;
+
+use UKFast\Page;
+use UKFast\Account\Entities\Contact;
+
+class ContactClient extends Client
+{
+    /**
+     * Gets a paginated response of all Contacts
+     *
+     * @param int $page
+     * @param int $perPage
+     * @param array $filters
+     * @return Page
+     */
+    public function getAll($page = 1, $perPage = 15, $filters = [])
+    {
+        $page = $this->paginatedRequest('v1/contacts', $page, $perPage, $filters);
+        $page->serializeWith(function ($item) {
+            return new Contact($item);
+        });
+
+        return $page;
+    }
+
+    /**
+     * Gets an individual contact
+     *
+     * @param string $id
+     * @return Contact
+     */
+    public function getById($id)
+    {
+        $response = $this->request("GET", "v1/contacts/$id");
+        $body = $this->decodeJson($response->getBody()->getContents());
+        return new Contact($body->data);
+    }
+}
