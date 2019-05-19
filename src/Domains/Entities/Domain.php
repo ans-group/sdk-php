c23a3c5e1f6181734a86f663a6fee2d6310f2dee
Gavin Taylor <gavin@gavtaylor.uk>
2019-05-19 20:09:07 +0100

    split domain client into entity clients

---
+++ src/Domains/Entities/Domain.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace UKFast\Domains\Entities;
+
+class Domain
+{
+    public $name;
+    public $status;
+
+    public $registrar;
+
+    public $registeredAt;
+    public $updatedAt;
+    public $renewalAt;
+
+    public $autoRenew;
+    public $whoisPrivacy;
+
+
+    /**
+     * Domain constructor.
+     * @param null $item
+     */
+    public function __construct($item = null)
+    {
+        if (empty($item)) {
+            return;
+        }
+
+        $this->name = $item->name;
+        $this->status = $item->status;
+
+        $this->registrar = $item->registrar;
+
+        $this->registeredAt = $item->registered_at;
+        $this->updatedAt = $item->updated_at;
+        $this->renewalAt = $item->renewal_at;
+
+        $this->autoRenew = $item->auto_renew;
+        $this->whoisPrivacy = $item->whois_privacy;
+    }
+}
