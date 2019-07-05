fcefece2ee1014545f8211f90b6883f6ed468d4a
ryan.bibby <ryan.bibby@ukfast.co.uk>
2019-07-05 09:35:21 +0100

    Rename test methods to be more accurate

--- tests/PssClientTest.php
+++ tests/PssClientTest.php
@@ -77,7 +77,7 @@ class PssClientTest extends TestCase
     /**
      * @test
      */
-    public function gets_a_reply()
+    public function gets_replies()
     {
         $mock = new MockHandler([
             new Response(200, [], json_encode([
@@ -127,7 +127,7 @@ class PssClientTest extends TestCase
     /**
      * @test
      */
-    public function gets_a_reply_with_attachments()
+    public function gets_replies_with_attachments()
     {
         $mock = new MockHandler([
             new Response(200, [], json_encode([
