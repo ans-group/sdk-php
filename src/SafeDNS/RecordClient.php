bae7eb16814201e89d8177e59f14fcb35671147c
Gavin Taylor <gavin@gavtaylor.uk>
2019-07-17 21:03:31 +0100

    add basic support for creating/updating/deleting safeDNS records

--- src/SafeDNS/RecordClient.php
+++ src/SafeDNS/RecordClient.php
@@ -12,21 +12,6 @@ class RecordClient extends Client
 
 
     /**
-     * Gets an individual Record
-     *
-     * @param string $id
-     * @return Record
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     */
-    public function getById($id)
-    {
-        $response = $this->request("GET", "v1/records/$id");
-        $body = $this->decodeJson($response->getBody()->getContents());
-        return new Record($body->data);
-    }
-
-
-    /**
      * Get records by zone name
      * @param $zoneName
      * @param int $page
@@ -38,7 +23,8 @@ class RecordClient extends Client
     public function getByZoneName($zoneName, $page = 1, $perPage = 15, $filters = [])
     {
         $page = $this->paginatedRequest('v1/zones/'.$zoneName.'/records', $page, $perPage, $filters);
-        $page->serializeWith(function ($item) {
+        $page->serializeWith(function ($item) use ($zoneName) {
+            $item->zone = $zoneName;
             return new Record($item);
         });
 
@@ -77,5 +63,88 @@ class RecordClient extends Client
         }
 
         return $items;
+    }
+
+    /**
+     * Gets a specific zone record by ID
+     *
+     * @param $zoneName
+     * @param string $id
+     * @return Record
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getZoneRecordById($zoneName, $id)
+    {
+        $response = $this->request("GET", "v1/zones/$zoneName/records/$id");
+        $body = $this->decodeJson($response->getBody()->getContents());
+
+        // zone isnt currently returned by the api
+        $body->data->zone = $zoneName;
+
+        return new Record($body->data);
+    }
+
+    /**
+     * Create a new record
+     *
+     * @param Record $record
+     * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function create(Record $record)
+    {
+        $data = [
+            'name' => $record->name,
+            'type' => strtoupper($record->type),
+            'content' => $record->content,
+        ];
+
+        $response = $this->post("v1/zones/".$record->zone."/records", json_encode($data), [
+            'Content-Type' => 'application/json'
+        ]);
+
+        if ($response->getStatusCode() != 201) {
+            throw new UKFastException('unexpected response code: ' . $response->getStatusCode());
+        }
+
+        $body = $this->decodeJson($response->getBody()->getContents());
+
+        $record->id = $body->data->id;
+
+        return $record;
+    }
+
+    /**
+     * Update an existing record
+     *
+     * @param Record $record
+     * @return bool
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function update(Record $record)
+    {
+        $data = [
+            'name' => $record->name,
+            'content' => $record->content,
+        ];
+
+        $response = $this->patch("v1/zones/".$record->zone."/records/".$record->id."", json_encode($data), [
+            'Content-Type' => 'application/json'
+        ]);
+
+        return $response->getStatusCode() == 200;
+    }
+
+    /**
+     * Delete a record
+     *
+     * @param Record $record
+     * @return bool
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function destroy(Record $record)
+    {
+        $response = $this->delete("v1/zones/".$record->zone."/records/".$record->id."");
+        return $response->getStatusCode() == 204;
     }
 }
