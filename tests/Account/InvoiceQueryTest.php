6b906b020d2b3e8ea277a3a981e2bbc4f032dcdc
Tyler Woonton <tylerwoonton11@gmail.com>
2019-08-13 16:34:30 +0100

    Added Invoice and InvoiceQuery to PHP SDK

---
+++ tests/Account/InvoiceQueryTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Account;
+
+use DateTime;
+use GuzzleHttp\Client as Guzzle;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\TestCase;
+use UKFast\SDK\Account\Entities\InvoiceQuery;
+
+class InvoiceQueryTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function get_invoice_query_by_id()
+    {
+        $mock = new MockHandler([
+            new Response(200, [], json_encode([
+                "data" => [
+                    "id" => 1,
+                    "contact_id" => 1,
+                    "amount" => 5000,
+                    "what_was_expected" => "I didn't get the full amount",
+                    "what_was_received" => "4500",
+                    "proposed_solution" => "Resolve the difference",
+                    "invoice_ids" => [
+                            2514571,
+                            2456789,
+                            1245678
+                    ]
+                ],
+                "meta" => []
+            ])),
+        ]);
+
+        $handler = HandlerStack::create($mock);
+        $guzzle = new Guzzle(['handler' => $handler]);
+
+        $id = 1;
+
+        $client = new \UKFast\SDK\Account\Client($guzzle);
+        $invoiceQuery = $client->invoiceQueries()->getById($id);
+
+        $this->assertTrue($invoiceQuery instanceof \UKFast\SDK\Account\Entities\InvoiceQuery);
+        $this->assertEquals($id, $invoiceQuery->id);
+        $this->assertEquals(1, $invoiceQuery->contactId);
+        $this->assertEquals(5000, $invoiceQuery->amount);
+        $this->assertEquals("I didn't get the full amount", $invoiceQuery->whatWasExpected);
+        $this->assertEquals("4500", $invoiceQuery->whatWasReceived);
+        $this->assertEquals("Resolve the difference", $invoiceQuery->proposedSolution);
+        $this->assertEquals([2514571, 2456789, 1245678], $invoiceQuery->invoiceIds);
+    }
+}
