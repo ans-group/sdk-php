7ec869102883295b827f54d962068538a01fd09f
paul.mcnally <paul.mcnally@ukfast.co.uk>
2019-09-26 12:58:52 +0100

    test for solution get datastores

--- tests/eCloud/SolutionTest.php
+++ tests/eCloud/SolutionTest.php
@@ -59,4 +59,67 @@ class SolutionTest extends TestCase
         $this->assertEquals('Hybrid', $solution->environment);
         $this->assertEquals(1, $solution->podId);
     }
+
+    /**
+     * @test
+     */
+    public function get_datastores()
+    {
+        $mock = new MockHandler([
+            new Response(200, [], json_encode([
+                'data' => [
+                    [
+                        'id' => 1,
+                        'name' => 'Datastore 1',
+                        'status' => 'Completed',
+                        'capacity' => 700,
+                        'allocated' => null,
+                        'available' => null,
+                        'solution_id' => 1,
+                        'site_id' => null,
+                    ],
+                    [
+                        'id' => 1,
+                        'name' => 'Datastore 2',
+                        'status' => 'Expanding',
+                        'capacity' => 500,
+                        'allocated' => null,
+                        'available' => null,
+                        'solution_id' => 1,
+                        'site_id' => null,
+                    ],
+                ],
+                "meta" => [
+                    "pagination" => [
+                        "total" => 2,
+                        "count" => 2,
+                        "per_page" => 100,
+                        "current_page" => 1,
+                        "total_pages" => 1,
+                        "links" => []
+                    ]
+                ]
+            ])),
+        ]);
+
+        $handler = HandlerStack::create($mock);
+        $guzzle = new Guzzle(['handler' => $handler]);
+
+        $client = new \UKFast\SDK\eCloud\Client($guzzle);
+        $solutionId = 1;
+        $page = $client->solutions()->getDatastores($solutionId);
+
+        $this->assertTrue($page instanceof \UKFast\SDK\Page);
+        $request = $page->getItems()[0];
+
+        $this->assertTrue($request instanceof \UKFast\SDK\eCloud\Entities\Datastore);
+        $this->assertEquals(1, $request->id);
+        $this->assertEquals('Datastore 1', $request->name);
+        $this->assertEquals('Completed', $request->status);
+        $this->assertEquals(700, $request->capacity);
+        $this->assertNull($request->allocated);
+        $this->assertNull($request->available);
+        $this->assertEquals(1, $request->solutionId);
+        $this->assertNull($request->siteId);
+    }
 }
