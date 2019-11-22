42414f6a5be59e39b83b232fe1a03c2d0e76e3cb
George Kelly <george.kelly@ukfast.co.uk>
2019-11-22 13:24:13 +0000

    Made attachment and product extend entity

--- src/PSS/Entities/Product.php
+++ src/PSS/Entities/Product.php
@@ -2,29 +2,12 @@
 
 namespace UKFast\SDK\PSS\Entities;
 
-class Product
+use UKFast\SDK\Entity;
+
+/**
+ * @property int $id
+ * @property string $type
+ */
+class Product extends Entity
 {
-    /**
-     * @var int
-     */
-    public $id;
-
-    /**
-     * @var string
-     */
-    public $type;
-
-    /**
-     * Product constructor.
-     * @param $item
-     */
-    public function __construct($item = null)
-    {
-        if (is_null($item)) {
-            return;
-        }
-        
-        $this->id = $item->id;
-        $this->type = $item->type;
-    }
 }
