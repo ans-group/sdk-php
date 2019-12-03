f847371893d8aaf0ed242a063b2599f1f322d74d
MisterCoder <mrcoder@hotmail.com>
2019-12-03 07:20:16 +0000

    test cleanup

--- tests/eCloud/ApplianceVersionDataTest.php
+++ tests/eCloud/ApplianceVersionDataTest.php
@@ -13,27 +13,12 @@ class ApplianceVersionDataTest extends TestCase
     public function testGetApplianceVersionData()
     {
         $data = [
-            'id' => 'ccd66630-a25e-4281-9042-e83ff71c371e',
-            'appliance_id' => '2bb38535-9f11-49b0-b6dd-37e405b57cd8',
-            'version' => 2,
-            'script_template' => 'cat /var/www/html/wp-config.php',
-            'vm_template' => 'centos7-wordpress-v1.0.0',
-            'active' => true,
-            'created_at' => '2019-03-14T11:41:18+00:00',
-            'updated_at' => '2019-03-14T11:41:18+00:00',
-        ];
-        $mock = new MockHandler([new Response(200, [], json_encode(['data' => $data, 'meta' => []]))]);
-        $client = new \UKFast\SDK\eCloud\Client(new Guzzle(['handler' => HandlerStack::create($mock)]));
-        $applianceVersion = $client->appliance()->getVersion($data['appliance_id']);
-        $this->assertTrue($applianceVersion instanceof \UKFast\SDK\eCloud\Entities\ApplianceVersion);
-
-        $data = [
             'key' => 'key',
             'value' => 'value',
         ];
         $mock = new MockHandler([new Response(200, [], json_encode(['data' => $data, 'meta' => []]))]);
         $client = new \UKFast\SDK\eCloud\Client(new Guzzle(['handler' => HandlerStack::create($mock)]));
-        $applianceVersionData = $client->applianceVersion()->getDataByKey($applianceVersion->id, $data['key']);
+        $applianceVersionData = $client->applianceVersion()->getDataByKey(null, $data['key']);
         $this->assertTrue($applianceVersionData instanceof \UKFast\SDK\eCloud\Entities\Appliance\Version\Data);
         foreach (array_keys($data) as $key) {
             $modelKey = lcfirst(str_replace('_', '', ucwords($key, '_')));
