9dee86f2f226730d343564050c50380153b873cd
George Kelly <george.kelly@ukfast.co.uk>
2019-12-17 15:03:30 +0000

    Added loadbalancer entity and client

---
+++ src/Loadbalancers/Entities/Loadbalancer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace UKFast\SDK\Loadbalancers\Entities;
+
+use UKFast\SDK\Entity;
+
+/**
+ * @property string $id
+ * @property string $name
+ * @property string $hostname
+ */
+class Loadbalancer extends Entity
+{
+}
