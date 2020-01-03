ee1d9e33a71d2f25cbf0b7cbbc7d73673cef7ba7
Phil Young <philip.young@ukfast.co.uk>
2020-01-03 15:42:34 +0000

    Add Paginated Response For Account Credits
    First step in removing the legacy "getAll"

--- src/Account/CreditClient.php
+++ src/Account/CreditClient.php
@@ -10,6 +10,17 @@ class CreditClient extends Client
     protected $basePath = 'account/';
 
     /**
+     * Maps entity property names to API names
+     *
+     * @var array
+     */
+    protected $requestMap = [
+        'type'      => 'type',
+        'total'     => 'total',
+        'remaining' => 'remaining',
+    ];
+
+    /**
      * Gets account credits
      *
      * @return Credit Array
@@ -23,5 +34,42 @@ class CreditClient extends Client
         return array_map(function ($item) {
             return new Credit($item);
         }, $body->data);
+    }
+
+    /**
+     * Gets a paginated response of credits
+     *
+     * @param int   $page
+     * @param int   $perPage
+     * @param array $filters
+     * @return \UKFast\SDK\Page
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getPage($page = 1, $perPage = 20, $filters = [])
+    {
+        $filters = $this->friendlyToApi($filters, $this->requestMap);
+
+        $page = $this->paginatedRequest('v1/credits', $page, $perPage, $filters);
+
+        $page->serializeWith(function ($item) {
+            return $this->serializeCredit($item);
+        });
+
+        return $page;
+    }
+
+    /**
+     * Maps an API credit to a credit entity
+     *
+     * @param $item
+     * @return Credit
+     */
+    public function serializeCredit($item)
+    {
+        return new Credit([
+            'type'      => $item->type,
+            'total'     => $item->total,
+            'remaining' => $item->remaining,
+        ]);
     }
 }
