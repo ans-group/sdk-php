b0ca4bccdbfd237a4fa71827a238b2fe67b500ec
Martin Shaw <martin.shaw@ukfast.co.uk>
2020-01-20 15:39:04 +0000

    Add required Record client methods

---
+++ src/DDoSX/Entities/Record.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace UKFast\SDK\DDoSX\Entities;
+
+use UKFast\SDK\Entity;
+
+/**
+ * @property int $id
+ * @property string $domainName
+ * @property int $safednsRecordId
+ * @property int $sslId
+ * @property string $name
+ * @property string $type
+ * @property string $content
+ */
+class Record extends Entity
+{
+    //
+}
---
+++ src/DDoSX/RecordClient.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace UKFast\SDK\DDoSX;
+
+use UKFast\SDK\Client as BaseClient;
+use UKFast\SDK\DDoSX\Entities\Record;
+
+class RecordClient extends BaseClient
+{
+    protected $basePath = 'ddosx/';
+
+    /**
+     * @param $domainName
+     * @param int $page
+     * @param int $perPage
+     * @param array $filters
+     * @return int|\UKFast\SDK\Page
+     */
+    public function getPage($domainName, $page = 1, $perPage = 20, $filters = [])
+    {
+        $page = $this->paginatedRequest('v1/domains/' . $domainName . '/records', $page, $perPage, $filters);
+        $page->serializeWith(function ($item) {
+            return new Record($item);
+        });
+
+        return $page;
+    }
+
+    /**
+     * @param $domainName
+     * @param $recordId
+     * @return UKFast\SDK\DDoSX\Entities\Record
+     */
+    public function getById($domainName, $recordId)
+    {
+        $response = $this->request("GET", 'v1/domains/' . $domainName . '/records/' . $recordId);
+        $body = $this->decodeJson($response->getBody()->getContents());
+
+        return new Record($body->data);
+    }
+
+    /**
+     * Delete a record
+     *
+     * @param Record $record
+     * @return bool
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function destroy(Record $record)
+    {
+        $response = $this->delete("v1/domains/".$record->domainName."/records/".$record->id);
+
+        return $response->getStatusCode() == 204;
+    }
+}
