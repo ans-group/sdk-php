ff6313e3ae6a3ef93685e826ca9a5618ed9df97e
Martin Shaw <martin.shaw@ukfast.co.uk>
2020-01-21 16:04:56 +0000

    Revert unmerged changes from current master branch

--- src/DDoSX/RecordClient.php
+++ src/DDoSX/RecordClient.php
@@ -22,16 +22,16 @@ class RecordClient extends BaseClient
     ];
 
     /**
-     * Get paginated response of records
-     *
-     * @param int   $page
-     * @param int   $perPage
+     * @param int $page
+     * @param int $perPage
      * @param array $filters
      * @return int|\UKFast\SDK\Page
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function getPage($page = 1, $perPage = 20, $filters = [])
     {
         $filters = $this->friendlyToApi($filters, $this->requestMap);
+
         $page = $this->paginatedRequest('v1/records', $page, $perPage, $filters);
         $page->serializeWith(function ($item) {
             return new Record($this->apiToFriendly($item, $this->requestMap));
@@ -41,37 +41,6 @@ class RecordClient extends BaseClient
     }
 
     /**
-     * Get singular records by its Id
-     *
-     * @param $domainName
-     * @param $recordId
-     * @return Record
-     */
-    public function getById($domainName, $recordId)
-    {
-        $response = $this->request("GET", 'v1/domains/' . $domainName . '/records/' . $recordId);
-        $body = $this->decodeJson($response->getBody()->getContents());
-
-        return new Record($this->apiToFriendly($body, $this->requestMap));
-    }
-
-    /**
-     * Delete a record
-     *
-     * @param Record $record
-     * @return bool
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     */
-    public function destroy(Record $record)
-    {
-        $response = $this->delete("v1/domains/".$record->domain_name."/records/".$record->id);
-
-        return $response->getStatusCode() == 204;
-    }
-  
-    /**
-     * Get paginated response of records associated with a domain name
-     *
      * @param $domainName
      * @param int $page
      * @param int $perPage
