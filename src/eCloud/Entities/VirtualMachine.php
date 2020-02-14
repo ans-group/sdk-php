090540cfd403e9469cd5464eb774dfdebe03e586
Gavin Taylor <gavin@gavtaylor.uk>
2020-02-14 11:16:08 +0000

    update VirtualMachine entity hydration to current standards

--- src/eCloud/Entities/VirtualMachine.php
+++ src/eCloud/Entities/VirtualMachine.php
@@ -2,87 +2,45 @@
 
 namespace UKFast\SDK\eCloud\Entities;
 
-class VirtualMachine
+use UKFast\SDK\Entity;
+
+/**
+ * Class VirtualMachine
+ * @package UKFast\eCloud\Entities
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $status
+ *
+ * @property string $hostname
+ * @property string $computerName
+ *
+ * @property string $cpu
+ * @property string $ram
+ *
+ * @property string $hdd
+ * @property string $disks
+ * @property string $datastoreId
+ *
+ * @property string $ipAddresses
+ *
+ * @property string $template
+ * @property string $platform
+ *
+ * @property string $backup
+ * @property string $support
+ *
+ * @property string $environment
+ * @property string $solutionId
+ * @property string $adDomainId
+ *
+ * @property string $power
+ * @property string $tools
+ *
+ * @property string $credentials
+ */
+
+class VirtualMachine extends Entity
 {
-    public $id;
-    public $name;
-    public $status;
 
-    public $computerName;
-    public $hostname;
-
-    public $cpu;
-    public $ram;
-
-    public $hdd;
-    public $disks;
-    public $datastoreId;
-
-    public $ipAddresses;
-
-    public $template;
-    public $platform;
-
-    public $backup;
-    public $support;
-
-    public $environment;
-    public $solutionId;
-
-    public $power;
-    public $tools;
-
-    public $credentials;
-
-    /**
-     * VirtualMachine constructor.
-     * @param null $item
-     */
-    public function __construct($item = null)
-    {
-        if (empty($item)) {
-            return;
-        }
-
-        $this->environment = $item->environment;
-        $this->solutionId = $item->solution_id;
-
-        $this->id = $item->id;
-        $this->name = $item->name;
-        $this->status = $item->status;
-
-        $this->computerName = $item->computername;
-        $this->hostname = $item->hostname;
-
-        $this->cpu = $item->cpu;
-        $this->ram = $item->ram;
-
-        $this->hdd = $item->hdd;
-        if (isset($item->hdd_disks)) {
-            $this->disks = array_map(function ($item) {
-                return new Hdd($item);
-            }, $item->hdd_disks);
-        }
-
-
-        $this->ipAddresses = (object) [
-            'internal' => $item->ip_internal,
-            'external' => $item->ip_external,
-        ];
-
-        $this->template = $item->template;
-        $this->platform = $item->platform;
-
-        $this->backup = $item->backup;
-        $this->support = $item->support;
-
-
-        if (isset($item->power_status)) {
-            $this->power = $item->power_status;
-        }
-
-        if (isset($item->power_status)) {
-            $this->tools = $item->tools_status;
-        }
-    }
 }
