0b198ece77a3d3fdb36afb74b03edd5d49688be7
Martin Shaw <martin.shaw@ukfast.co.uk>
2020-02-21 15:51:30 +0000

    Add ValidationResult entity class

---
+++ src/SSL/Entities/ValidationResult.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace UKFast\SDK\SSL\Entities;
+
+use UKFast\SDK\Entity;
+
+/**
+ * @property string $certificate
+ * @property string $certificateKey
+ * @property string $caBundle
+ */
+class ValidationResult extends Entity
+{
+    //
+}
