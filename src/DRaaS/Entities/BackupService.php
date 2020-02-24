e3d376371ebacb3093402e92fadc7cc4d4ab8159
paul.mcnally <paul.mcnally@ukfast.co.uk>
2020-02-24 11:46:10 +0000

    Get information about the backup service assigned to the solution

---
+++ src/DRaaS/Entities/BackupService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace UKFast\SDK\DRaaS\Entities;
+
+use UKFast\SDK\Entity;
+
+/**
+ * Class Backup Service
+ * @property string $service
+ * @property string $account_name
+ */
+class BackupService extends Entity
+{
+}
