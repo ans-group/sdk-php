7eec303c7c6e323dc0949580f2d94ae50c8206c3
Andrew Hughes <andrew.hughes@ukfast.co.uk>
2020-02-26 09:45:20 +0000

    add type entity

---
+++ src/Billing/Entities/Type.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace UKFast\SDK\Billing\Entities;
+
+use UKFast\SDK\Entity;
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $category
+ */
+class Type extends Entity
+{
+    //
+}
