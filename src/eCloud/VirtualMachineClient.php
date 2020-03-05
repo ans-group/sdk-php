84a6d3f81e5ead7c50bbe29701bce90b91163276
Gavin Taylor <gavin@gavtaylor.uk>
2020-03-05 13:21:00 +0000

    add backup flag to VirtualMachineClient::create for Public VMs

--- src/eCloud/VirtualMachineClient.php
+++ src/eCloud/VirtualMachineClient.php
@@ -171,12 +171,8 @@ class VirtualMachineClient extends Client
 
 
         // additional options
-        if ($virtualMachine->encryptionRequired == true) {
-            $data['encrypt'] = true;
-        }
-
-        if (!empty($virtualMachine->adDomainId)) {
-            $data['ad_domain_id'] = true;
+        if (!empty($virtualMachine->computerName)) {
+            $data['computername'] = $virtualMachine->computerName;
         }
 
         if (!empty($virtualMachine->sshKeys)) {
@@ -187,10 +183,17 @@ class VirtualMachineClient extends Client
             $data['tags'] = $virtualMachine->tags;
         }
 
-        if (!empty($virtualMachine->computerName)) {
-            $data['computername'] = $virtualMachine->computerName;
+        if (!empty($virtualMachine->adDomainId)) {
+            $data['ad_domain_id'] = true;
         }
 
+        if ($virtualMachine->encryptionRequired == true) {
+            $data['encrypt'] = true;
+        }
+
+        if ($virtualMachine->backup == true) {
+            $data['backup'] = true;
+        }
 
         $response = $this->post("v1/vms", json_encode($data), [
             'Content-Type' => 'application/json'
