de47a02091ba3bf04ef5a24473e2fd965438c9fe
Ben Hartley <ben.hartley@ukfast.co.uk>
2020-07-15 16:48:03 +0100

    added getById method

--- src/DDoSX/WafLogMatchClient.php
+++ src/DDoSX/WafLogMatchClient.php
@@ -26,6 +26,22 @@ class WafLogMatchClient extends BaseClient
         "uri_part" => "uriPart"
     ];
 
+    /**
+     * Gets a waf matches for a request
+     *
+     * @param string $requestId
+     */
+    public function getById($requestId, $page = 1, $perPage = 15, $filters = [])
+    {
+        $page = $this->paginatedRequest('v1/waf/logs/' . $requestId . '/matches', $page, $perPage, $filters);
+        
+        $page->serializeWith(function ($item) {
+            return new WafMatch($this->apiToFriendly($item, $this->requestMap));
+        });
+        
+        return $page;
+    }
+
      /**
      * Gets a waf log match from a request
      *
