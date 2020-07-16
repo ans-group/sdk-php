fc6d561190786eaff2b4d4bc0f1c239607647b8a
Martin Shaw <martin.shaw@ukfast.co.uk>
2020-07-16 13:01:36 +0100

    Add remaining DDoSX Entity classes

---
+++ src/DDoSX/Entities/HstsRule.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace UKFast\SDK\DDoSX\Entities;
+
+use UKFast\SDK\Entity;
+
+/**
+ * @property string $id
+ * @property int $maxAge
+ * @property bool $includeSubdomains
+ * @property string $type
+ * @property string $recordName
+ */
+class HstsRule extends Entity
+{
+
+}
---
+++ src/DDoSX/Entities/SslCertificate.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace UKFast\SDK\DDoSX\Entities;
+
+use UKFast\SDK\Entity;
+
+/**
+ * @property string $certificate
+ * @property string $caBundle
+ */
+class SslCertificate extends Entity
+{
+
+}
