fb1afd85c1666834443272185d17e456ab2bc96f
Gavin Taylor <gavin.taylor@ukfast.co.uk>
2020-09-24 15:10:58 +0100

    surface basic instance managed credentials

--- src/eCloud/InstanceClient.php
+++ src/eCloud/InstanceClient.php
@@ -2,6 +2,7 @@
 
 namespace UKFast\SDK\eCloud;
 
+use UKFast\SDK\eCloud\Entities\Credentials;
 use UKFast\SDK\Entities\ClientEntityInterface;
 use UKFast\SDK\Traits\PageItems;
 use UKFast\SDK\eCloud\Entities\Instance;
@@ -28,5 +29,58 @@ class InstanceClient extends Client implements ClientEntityInterface
         return new Instance(
             $this->apiToFriendly($data, $this->getEntityMap())
         );
+    }
+
+    /**
+     * Get array of instance credentials
+     * @param $id
+     * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getCredentials($id)
+    {
+        $currentPage = 1;
+        $perPage = 15;
+
+        $page = $this->paginatedRequest(
+            $this->collectionPath . '/' . $id . '/credentials',
+            $currentPage,
+            $perPage
+        );
+
+        if ($page->totalItems() == 0) {
+            return [];
+        }
+
+        $credentialsClient = new CredentialsClient;
+
+        $page->serializeWith(function ($item) use ($credentialsClient) {
+            return $credentialsClient->loadEntity($item);
+        });
+
+        $items = $page->getItems();
+        if ($page->totalPages() == 1) {
+            return $items;
+        }
+
+        // get any remaining pages
+        while ($page->pageNumber() < $page->totalPages()) {
+            $page = $this->paginatedRequest(
+                $this->collectionPath . '/' . $id . '/credentials',
+                $currentPage++,
+                $perPage
+            );
+
+            $page->serializeWith(function ($item) use ($credentialsClient) {
+                return $credentialsClient->loadEntity($item);
+            });
+
+            $items = array_merge(
+                $items,
+                $page->getItems()
+            );
+        }
+
+        return $items;
     }
 }
