5dbb055d8f5f66289dd2b973b88e13e615fdb0e7
Gavin Taylor <gavin.taylor@ukfast.co.uk>
2020-11-10 15:26:21 +0000

    surface initial firewall rule port client

--- src/eCloud/Entities/FirewallRulePort.php
+++ src/eCloud/Entities/FirewallRulePort.php
@@ -7,17 +7,14 @@ use UKFast\SDK\Entity;
 /**
  * @property string $id
  * @property string $name
- * @property string $policyId
+ * @property string $ruleId
+ * @property string $protocol
  * @property string $source
  * @property string $destination
- * @property string $action
- * @property string $direction
- * @property int $sequence
- * @property bool $enabled
  * @property string $createdAt
  * @property string $updatedAt
  */
-class FirewallRule extends Entity
+class FirewallRulePort extends Entity
 {
     protected $dates = ['createdAt', 'updatedAt'];
 }
--- src/eCloud/FirewallRulePortClient.php
+++ src/eCloud/FirewallRulePortClient.php
@@ -4,26 +4,23 @@ namespace UKFast\SDK\eCloud;
 
 use UKFast\SDK\Entities\ClientEntityInterface;
 use UKFast\SDK\Traits\PageItems;
-use UKFast\SDK\eCloud\Entities\FirewallRule;
+use UKFast\SDK\eCloud\Entities\FirewallRulePort;
 
-class FirewallRuleClient extends Client implements ClientEntityInterface
+class FirewallRulePortClient extends Client implements ClientEntityInterface
 {
     use PageItems;
 
-    protected $collectionPath = 'v2/firewall-rules';
+    protected $collectionPath = 'v2/firewall-rule-ports';
 
     public function getEntityMap()
     {
         return [
             'id' => 'id',
             'name' => 'name',
-            'firewall_policy_id' => 'policyId',
+            'firewall_rule_id' => 'ruleId',
+            'protocol' => 'protocol',
             'source' => 'source',
             'destination' => 'destination',
-            'action' => 'action',
-            'direction' => 'direction',
-            'sequence' => 'sequence',
-            'enabled' => 'enabled',
             'created_at' => 'createdAt',
             'updated_at' => 'updatedAt',
         ];
@@ -31,7 +28,7 @@ class FirewallRuleClient extends Client implements ClientEntityInterface
 
     public function loadEntity($data)
     {
-        return new FirewallRule(
+        return new FirewallRulePort(
             $this->apiToFriendly($data, $this->getEntityMap())
         );
     }
