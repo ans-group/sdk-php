73323d156a14b637d3c3d65ddb74951eb246edfd
John Birch-Evans <jbirchevans@ukfast.co.uk>
2021-01-13 10:16:18 +0000

    Add the method to get the elevated privileges

--- src/ThreatMonitoring/Reporting/LoginHistoryClient.php
+++ src/ThreatMonitoring/Reporting/LoginHistoryClient.php
@@ -8,6 +8,7 @@ namespace UKFast\SDK\ThreatMonitoring\Reporting;
 
 use UKFast\SDK\Page;
 use UKFast\SDK\ThreatMonitoring\Client;
+use UKFast\SDK\ThreatMonitoring\Entities\ElevatedPrivileges;
 use UKFast\SDK\ThreatMonitoring\Entities\LoginRecord;
 
 class LoginHistoryClient extends Client
@@ -16,6 +17,10 @@ class LoginHistoryClient extends Client
         'full_log' => 'fullLog',
         'source_user' => 'sourceUser',
         'target_user' => 'targetUser'
+    ];
+    
+    const ELEVATED_PRIVILEGES_MAP = [
+        'source_user' => 'sourceUser'
     ];
     
     /**
@@ -36,8 +41,31 @@ class LoginHistoryClient extends Client
         return $page;
     }
     
+    /**
+     * Returns a list of when the user has elevated privileges from sudo to root
+     * @param int $page
+     * @param int $perPage
+     * @param array $filters
+     * @return Page
+     */
+    public function elevatedPrivileges($page = 1, $perPage = 15, $filters = [])
+    {
+        $page = $this->paginatedRequest('v1/reports/logged-in-users/elevated-privileges', $page, $perPage, $filters);
+    
+        $page->serializeWith(function ($item) {
+            return $this->serializeElevatedPrivileges($item);
+        });
+    
+        return $page;
+    }
+    
     public function serializeLoginTimeline($data)
     {
-        return new LoginRecord($this->apiToFriendly($data, static::LOGIN_RECORD_MAP));
+        return new LoginRecord($this->apiToFriendly($data, self::LOGIN_RECORD_MAP));
+    }
+    
+    public function serializeElevatedPrivileges($data)
+    {
+        return new ElevatedPrivileges($this->apiToFriendly($data, self::ELEVATED_PRIVILEGES_MAP));
     }
 }
