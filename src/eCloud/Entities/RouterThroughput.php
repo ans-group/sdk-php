3108e9bfe728a940e0ae5041c5cfc2f7acbc4996
Gavin Taylor <gavin.taylor@ukfast.co.uk>
2021-02-10 12:08:42 +0000

    surface initial RouterThroughput client

---
+++ src/eCloud/Entities/RouterThroughput.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace UKFast\SDK\eCloud\Entities;
+
+use UKFast\SDK\Entity;
+
+/**
+ * @property string $id
+ * @property string $name
+ * @property string $availabilityZoneId
+ * @property integer $committedBandwidth
+ * @property integer $burstSize
+ * @property string $createdAt
+ * @property string $updatedAt
+ */
+class RouterThroughput extends Entity
+{
+    protected $dates = ['createdAt', 'updatedAt'];
+}
