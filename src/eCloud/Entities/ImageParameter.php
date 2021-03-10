084124559052ca0ffc1d54463a1de714c31aa0ad
Gavin Taylor <gavin.taylor@ukfast.co.uk>
2021-03-10 16:49:39 +0000

    update image client to enable loading of image parameters and metadata

---
+++ src/eCloud/Entities/ImageParameter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace UKFast\SDK\eCloud\Entities;
+
+use UKFast\SDK\Entity;
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $key
+ * @property string $type
+ * @property string $description
+ * @property boolean $required
+ * @property string $validationRule
+ */
+class ImageParameter extends Entity
+{
+}
