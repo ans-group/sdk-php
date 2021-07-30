bb9c7ce900476fadcdf4886c8c5e0d903186462f
Ryan Bibby <ryan.bibby@ukfast.co.uk>
2021-07-30 10:19:42 +0100

    Add new timeouts properties

--- src/Loadbalancers/Entities/TargetGroup.php
+++ src/Loadbalancers/Entities/TargetGroup.php
@@ -16,6 +16,9 @@ use UKFast\SDK\Entity;
  * @property string $source
  * @property int $timeoutsConnect
  * @property int $timeoutsServer
+ * @property int $timeoutsHttpRequest
+ * @property int $timeoutsCheck
+ * @property int $timeoutsTunnel
  * @property string $monitorUrl
  * @property string $monitorMethod
  * @property string $monitorHost
