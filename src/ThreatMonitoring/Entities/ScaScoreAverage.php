328bdaee42188b6fbcc8e3baea08d013850af47b
Dawid Hochman <dawid@nue.digital>
2021-08-06 14:05:56 +0100

    - added SCA score average entity and endpoint

---
+++ src/ThreatMonitoring/Entities/ScaScoreAverage.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace UKFast\SDK\ThreatMonitoring\Entities;
+
+use UKFast\SDK\Entity;
+
+/**
+ * @property float $average
+ */
+class ScaScoreAverage extends Entity
+{
+}
--- src/ThreatMonitoring/Reporting/ScaClient.php
+++ src/ThreatMonitoring/Reporting/ScaClient.php
@@ -5,6 +5,7 @@ namespace UKFast\SDK\ThreatMonitoring\Reporting;
 
 use UKFast\SDK\ThreatMonitoring\Client;
 use UKFast\SDK\ThreatMonitoring\Entities\ScaResults;
+use UKFast\SDK\ThreatMonitoring\Entities\ScaScoreAverage;
 use UKFast\SDK\ThreatMonitoring\Entities\ScaSuggestionResults;
 use UKFast\SDK\ThreatMonitoring\Entities\TopAlertsList;
 use UKFast\SDK\ThreatMonitoring\Entities\TopAlertsType;
@@ -14,6 +15,9 @@ class ScaClient extends Client
     const SUGGESTION_MAP = [];
 
     const INDIVIDUAL_MAP = [];
+
+    const SCA_SCORE_AVERAGE_MAP = [];
+
     /**
      * Get Sca Data
      *
@@ -41,5 +45,31 @@ class ScaClient extends Client
         $response = $this->get('v1/agents/' . $agentId . '/sca/checks/' . $policyId);
         $data = $this->decodeJson($response->getBody()->getContents());
         return new ScaSuggestionResults($this->apiToFriendly($data, self::INDIVIDUAL_MAP));
+    }
+
+    /**
+     * Get average sca scores for agents for a reseller.
+     *
+     * @param array $filters
+     * @return ScaScoreAverage
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getScaScoreAverage ($filters = []) : ScaScoreAverage
+    {
+        $queryParams = '';
+
+        if (count($filters)) {
+            $queryParams = '?' . http_build_query($filters);
+        }
+
+        $response = $this->get('v1/reports/sca-score/average' . $queryParams);
+
+        $body = $this->decodeJson($response->getBody()->getContents());
+
+        return $this->serializeScaScoreAverage($body->data);
+    }
+
+    public function serializeScaScoreAverage($data) {
+        return new ScaScoreAverage($this->apiToFriendly($data, static::SCA_SCORE_AVERAGE_MAP));
     }
 }
