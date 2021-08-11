55e3befec13aa0f20fcba58e4bb2c85f2650bca1
Paul McNally <paul.mcnally@ukfast.co.uk>
2021-08-11 09:55:32 +0100

    Adds vpnEndpoint and VpnProfileGroup

---
+++ src/eCloud/VpnEndpointClient.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace UKFast\SDK\eCloud;
+
+use UKFast\SDK\eCloud\Entities\VpnEndpoint;
+use UKFast\SDK\Entities\ClientEntityInterface;
+use UKFast\SDK\Traits\PageItems;
+
+class VpnEndpointClient extends Client implements ClientEntityInterface
+{
+    use PageItems;
+
+    protected $collectionPath = 'v2/vpn-endpoints';
+
+    public function getEntityMap()
+    {
+        return [
+            'id' => 'id',
+            'name' => 'name',
+            'floating_ip_id' => 'floatingIpId',
+            'created_at' => 'createdAt',
+            'updated_at' => 'updatedAt',
+        ];
+    }
+
+    public function loadEntity($data)
+    {
+        return new VpnEndpoint(
+            $this->apiToFriendly($data, $this->getEntityMap())
+        );
+    }
+}
