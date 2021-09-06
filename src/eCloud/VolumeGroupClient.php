5681490c81295d4ec1c60e4734d2a545dbaadfec
ptmcnally <30559829+ptmcnally@users.noreply.github.com>
2021-09-06 11:49:35 +0100

    236 crud for volume groups (#436)
    * volume group client
    
    * volume group client
    
    * volume group client
    
    Co-authored-by: Paul McNally <paul.mcnally@ukfast.co.uk>

---
+++ src/eCloud/VolumeGroupClient.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace UKFast\SDK\eCloud;
+
+use UKFast\SDK\eCloud\Entities\VolumeGroup;
+use UKFast\SDK\Entities\ClientEntityInterface;
+use UKFast\SDK\Traits\PageItems;
+
+class VolumeGroupClient extends Client implements ClientEntityInterface
+{
+    use PageItems;
+
+    protected $collectionPath = 'v2/volume-groups';
+
+    public function getEntityMap()
+    {
+        return [
+            'id' => 'id',
+            'name' => 'name',
+            'vpc_id' => 'vpcId',
+            'availability_zone_id' => 'availabilityZoneId',
+            'sync' => 'sync',
+            'created_at' => 'createdAt',
+            'updated_at' => 'updatedAt',
+        ];
+    }
+
+    public function loadEntity($data)
+    {
+        return new VolumeGroup(
+            $this->apiToFriendly($data, $this->getEntityMap())
+        );
+    }
+}
