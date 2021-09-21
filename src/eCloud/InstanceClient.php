d65c3189c9cdabfbed0f1a7f1bd7cf570f8de23b
Paul McNally <paul.mcnally@ukfast.co.uk>
2021-09-21 09:05:37 +0100

    surface volume group id on instances

--- src/eCloud/InstanceClient.php
+++ src/eCloud/InstanceClient.php
@@ -31,6 +31,7 @@ class InstanceClient extends Client implements ClientEntityInterface
             'agent_running' => 'agentRunning',
             'backup_enabled' => 'backupEnabled',
             'host_group_id' => 'hostGroupId',
+            'volume_group_id' => 'volumeGroupId',
             'created_at' => 'createdAt',
             'updated_at' => 'updatedAt',
         ];
