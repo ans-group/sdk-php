7f4276d99aa8cee1bbb121ec7fcb07bde819e8ab
George Kelly <george.kelly@ukfast.co.uk>
2022-03-24 10:19:02 +0000

    Setup cert client

---
+++ src/Loadbalancers/CertClient.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace UKFast\SDK\Loadbalancers;
+
+use UKFast\SDK\Client as BaseClient;
+use UKFast\SDK\Loadbalancers\Entities\Vip;
+
+class CertClient extends BaseClient
+{
+    protected $basePath = 'loadbalancers/';
+
+    const MAP = [
+        'listener_id' => 'listenerId',
+        'created_at' => 'createdAt',
+        'updated_at' => 'updatedAt',
+        'expires_at' => 'expiresAt',
+        'ca_bundle' => 'caBundle',
+    ];
+
+    /**
+     * Gets a paginated response of all Vips
+     *
+     * @param int $page
+     * @param int $perPage
+     * @param array $filters
+     * @return \UKFast\SDK\Page
+     */
+    public function getPage($page = 1, $perPage = 15, $filters = [])
+    {
+        $filters = $this->friendlyToApi($filters, self::MAP);
+        $page = $this->paginatedRequest('v2/certs', $page, $perPage, $filters);
+        $page->serializeWith(function ($item) {
+            return $this->serializeCert($item);
+        });
+
+        return $page;
+    }
+
+    /**
+     * @return \UKFast\SDK\Loadbalancers\Entities\Vip
+     */
+    public function serializeCert($raw)
+    {
+        return new Vip($this->apiToFriendly($raw, self::MAP));
+    }
+}
