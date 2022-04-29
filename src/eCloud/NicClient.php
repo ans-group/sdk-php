6961f111db284684e42a91da2a836b3853530a12
Tim Green <tim.green@ukfast.co.uk>
2022-04-29 13:37:30 +0100

    Added method to handle ip address detachment

--- src/eCloud/NicClient.php
+++ src/eCloud/NicClient.php
@@ -26,4 +26,63 @@ class NicClient extends Client implements ClientEntityInterface
             $this->apiToFriendly($data, $this->getEntityMap())
         );
     }
+  
+    /**
+     * Assigns an IP address with a NIC
+     * @param $id
+     * @param $ipAddressId
+     * @return bool
+     */
+    public function assignIpAddress($id, $ipAddressId)
+    {
+        $response = $this->post(
+            $this->collectionPath . '/' . $id . '/ip-addresses',
+            json_encode([ 'ip_address_id' => $ipAddressId ])
+        );
+        return $response->getStatusCode() == 202;
+    }
+
+    /**
+     * Detaches an IP address from a NIC
+     * @param $id
+     * @param $ipAddressId
+     * @return bool
+     */
+    public function detachIpAddress($id, $ipAddressId)
+    {
+        $response = $this->delete($this->collectionPath . '/' . $id . '/ip-addresses/' . $ipAddressId);
+        return $response->getStatusCode() == 202;
+    }
+  
+    /**
+     * Get the IP address records associated with a NIC
+     * @param $id
+     * @return array
+     */
+    public function getIpAddresses($id)
+    {
+        $page = $this->paginatedRequest(
+            $this->collectionPath . '/' . $id . '/ip-addresses',
+            $currentPage = 1,
+            $perPage = 15
+        );
+
+        if ($page->totalItems() == 0) {
+            return [];
+        }
+
+        $items = $page->getItems();
+        if ($page->totalPages() == 1) {
+            return $items;
+        }
+
+        while ($page->pageNumber() < $page->totalPages()) {
+            $page = $this->getPage($page->pageNumber() + 1, $perPage);
+            $items = array_merge(
+                $items,
+                $page->getItems()
+            );
+        }
+        return $items;
+    }
 }
