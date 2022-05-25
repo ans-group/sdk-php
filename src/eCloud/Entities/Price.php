a317eb13ae9579cd3d4c73647e29d8e1902a475d
Paul McNally <paul.mcnally@ukfast.co.uk>
2022-05-25 16:43:40 +0100

    get region prices

---
+++ src/eCloud/Entities/Price.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace UKFast\SDK\eCloud\Entities;
+
+use UKFast\SDK\Entity;
+
+/**
+ * @property string $availability_zone_id
+ * @property string $name
+ * @property string $category
+ * @property string $price
+ * @property string $rate
+ */
+class Price extends Entity
+{
+    public static $entityMap = [
+        'availability_zone_id' => 'availabilityZoneId',
+        'name' => 'name',
+        'category' => 'category',
+        'price' => 'price',
+        'rate' => 'rate',
+    ];
+}
--- src/eCloud/RegionClient.php
+++ src/eCloud/RegionClient.php
@@ -2,6 +2,7 @@
 
 namespace UKFast\SDK\eCloud;
 
+use UKFast\SDK\eCloud\Entities\Price;
 use UKFast\SDK\Entities\ClientEntityInterface;
 use UKFast\SDK\Traits\PageItems;
 use UKFast\SDK\eCloud\Entities\Region;
@@ -25,5 +26,42 @@ class RegionClient extends Client implements ClientEntityInterface
             'id' => 'id',
             'name' => 'name',
         ];
+    }
+
+    /**
+     * Get an array of all prices for the region
+     *
+     * @param array $filters
+     * @return array
+     */
+    public function getPrices($id, $filters = [])
+    {
+        $page = $this->paginatedRequest($this->collectionPath . '/' . $id . '/prices', 1, 15, $filters);
+
+        if ($page->totalItems() == 0) {
+            return [];
+        }
+
+        $loadEntity = function($data) {
+            return new Price($this->apiToFriendly($data, Price::$entityMap));
+        };
+
+        $page->serializeWith($loadEntity);
+
+        $items = $page->getItems();
+        if ($page->totalPages() == 1) {
+            return $items;
+        }
+
+        while ($page->pageNumber() < $page->totalPages()) {
+            $page = $this->getPage($page->pageNumber() + 1, 15, $filters);
+
+            $page->serializeWith($loadEntity);
+            $items = array_merge(
+                $items,
+                $page->getItems()
+            );
+        }
+        return $items;
     }
 }
