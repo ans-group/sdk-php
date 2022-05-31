d012fc211f0179c0534668b122e2ee63c07a1355
rawveg <rawveg@gmail.com>
2022-05-31 09:34:42 +0100

    Vpn session set pre shared key (#484)
    * Added method to allow setting of the pre-shared-key for a VPN Session
    
    Co-authored-by: Tim Green <tim.green@ans.co.uk>

--- src/eCloud/VpnSessionClient.php
+++ src/eCloud/VpnSessionClient.php
@@ -2,9 +2,10 @@
 
 namespace UKFast\SDK\eCloud;
 
-use UKFast\SDK\eCloud\Entities\VpnSession;
 use UKFast\SDK\eCloud\Entities\PreSharedKey;
+use UKFast\SDK\eCloud\Entities\VpnSession;
 use UKFast\SDK\Entities\ClientEntityInterface;
+use UKFast\SDK\SelfResponse;
 use UKFast\SDK\Traits\PageItems;
 
 class VpnSessionClient extends Client implements ClientEntityInterface
@@ -38,6 +39,12 @@ class VpnSessionClient extends Client implements ClientEntityInterface
         );
     }
 
+    /**
+     * Gets the PSK for a VPN Session
+     *
+     * @param int $id
+     * @return PreSharedKey
+     */
     public function getPsk($id)
     {
         $response = $this->get($this->collectionPath . '/' . $id . '/pre-shared-key');
@@ -45,5 +52,27 @@ class VpnSessionClient extends Client implements ClientEntityInterface
         return new PreSharedKey($this->apiToFriendly($body->data, [
             'psk' => 'psk'
         ]));
+    }
+
+    /**
+     * Sets the PSK for a VPN Session
+     *
+     * @param int $id
+     * @param PreSharedKey $entity
+     * @return SelfResponse
+     */
+    public function setPsk($id, PreSharedKey $entity)
+    {
+        $response = $this->put(
+            $this->collectionPath . '/' . $id . '/pre-shared-key',
+            json_encode($this->friendlyToApi($entity, ['psk' => 'psk']))
+        );
+        $responseBody = $this->decodeJson($response->getBody()->getContents());
+
+        return (new SelfResponse($responseBody))
+            ->setClient($this)
+            ->serializeWith(function ($responseBody) {
+                return $this->loadEntity($responseBody->data);
+            });
     }
 }
