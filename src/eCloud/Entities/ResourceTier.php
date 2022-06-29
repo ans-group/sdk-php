d9982e2e5926fb9a7fdc38037af29f27d10e99c5
Jay Pounder <jay.pounder@ukfast.co.uk>
2022-06-29 15:39:50 +0100

    fixed typo

--- src/eCloud/Entities/ResourceTier.php
+++ src/eCloud/Entities/ResourceTier.php
@@ -16,9 +16,9 @@ class ResourceTier extends Entity
     protected $dates = ['createdAt', 'updatedAt'];
 
     public static $entityMap = [
-        'id' => '$id',
-        'name' => '$name',
-        'availability_zone_id' => '$availabilityZoneId',
+        'id' => 'id',
+        'name' => 'name',
+        'availability_zone_id' => 'availabilityZoneId',
         'created_at' => 'createdAt',
         'updated_at' => 'updatedAt',
     ];
