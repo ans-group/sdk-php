77dd07813fd15a4c39e4c248b5d936928beb769d
dawid.hochman <dawid.hochman@ans.co.uk>
2023-02-09 15:00:57 +0000

    Added IOPS endpoint to eCloud Availability Zone client

--- src/eCloud/AvailabilityZoneClient.php
+++ src/eCloud/AvailabilityZoneClient.php
@@ -4,6 +4,7 @@ namespace UKFast\SDK\eCloud;
 
 use UKFast\SDK\eCloud\Entities\Product;
 use UKFast\SDK\eCloud\Entities\ResourceTier;
+use UKFast\SDK\eCloud\Entities\VPC\Iops;
 use UKFast\SDK\Entities\ClientEntityInterface;
 use UKFast\SDK\Traits\PageItems;
 use UKFast\SDK\eCloud\Entities\AvailabilityZone;
@@ -37,6 +38,13 @@ class AvailabilityZoneClient extends Client implements ClientEntityInterface
     {
         return $this->getChildResources($id, 'resource-tiers', function ($data) {
             return new ResourceTier($this->apiToFriendly($data, ResourceTier::$entityMap));
+        }, $filters);
+    }
+
+    public function getIops($id, $filters = [])
+    {
+        return $this->getChildResources($id, 'iops', function ($data) {
+            return new Iops($this->apiToFriendly($data, Iops::$entityMap));
         }, $filters);
     }
 }
