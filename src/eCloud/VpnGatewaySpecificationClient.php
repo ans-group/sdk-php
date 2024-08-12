838c0c407a441e79110bfd30684569d11b5377a7
dawid.hochman <dawid.hochman@ans.co.uk>
2024-08-12 09:57:03 +0100

    added eCloud VpnGatewaySpecifications

--- src/eCloud/VpnGatewaySpecificationClient.php
+++ src/eCloud/VpnGatewaySpecificationClient.php
@@ -2,6 +2,7 @@
 
 namespace UKFast\SDK\eCloud;
 
+use UKFast\SDK\eCloud\Entities\AvailabilityZone;
 use UKFast\SDK\eCloud\Entities\VpnGatewaySpecification;
 use UKFast\SDK\Entities\ClientEntityInterface;
 use UKFast\SDK\Traits\PageItems;
@@ -22,5 +23,12 @@ class VpnGatewaySpecificationClient extends Client implements ClientEntityInterf
         return new VpnGatewaySpecification(
             $this->apiToFriendly($data, VpnGatewaySpecification::$entityMap)
         );
+    }
+
+    public function getAvailabilityZones($id, $filters = [])
+    {
+        return $this->getChildResources($id, 'availability-zones', function ($data) {
+            return new AvailabilityZone($this->apiToFriendly($data, AvailabilityZone::$entityMap));
+        }, $filters);
     }
 }
