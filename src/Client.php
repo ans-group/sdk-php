a3ca54e6c7d737f17627ba9a070d0939c0ab8d6a
Phil Young <philip.young@ans.co.uk>
2025-08-08 16:17:46 +0100

    Fix a return type
    This should now return either this class the class that extends it

--- src/Client.php
+++ src/Client.php
@@ -53,7 +53,7 @@ class Client
      * Sets the API key to be used by the client
      *
      * @param string $token
-     * @return $this
+     * @return self
      */
     public function auth($token)
     {
