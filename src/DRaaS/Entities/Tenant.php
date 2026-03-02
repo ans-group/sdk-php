0cdcfbf34f3144c8f174fcf8724e0a23be28f8c3
dawid.hochman <dawid.hochman@ans.co.uk>
2026-03-02 11:04:36 +0000

    Add source property to DRaaS Tenant entity

--- src/DRaaS/Entities/Tenant.php
+++ src/DRaaS/Entities/Tenant.php
@@ -9,6 +9,7 @@ use UKFast\SDK\Entity;
  * @property string $id
  * @property string $name
  * @property object{id: string, name: string} $pod
+ * @property object{type: string, id: string} $source
  * @property object{status: string} $sync
  * @property DateTime $createdAt
  * @property DateTime $updatedAt
@@ -21,6 +22,7 @@ class Tenant extends Entity
         'id' => 'id',
         'name' => 'name',
         'pod' => 'pod',
+        'source' => 'source',
         'sync' => 'sync',
         'created_at' => 'createdAt',
         'updated_at' => 'updatedAt',
