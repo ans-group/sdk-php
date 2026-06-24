94ad9f011c5c28a518dcfa7bbc85a10d8692b556
dawid.hochman <dawid.hochman@ans.co.uk>
2026-06-24 09:27:17 +0100

    Deprecate stop() method and add cancel() method in RecoveryPlanClient

--- src/DRaaS/RecoveryPlanClient.php
+++ src/DRaaS/RecoveryPlanClient.php
@@ -31,11 +31,23 @@ class RecoveryPlanClient extends Client
      * @param string $recoveryPlanId
      * @return bool
      * @throws GuzzleException
+     * @deprecated Use cancel() instead
      */
     public function stop($recoveryPlanId)
     {
+        return $this->cancel($recoveryPlanId);
+    }
+
+    /**
+     * Cancel a recovery plan
+     * @param string $recoveryPlanId
+     * @return bool
+     * @throws GuzzleException
+     */
+    public function cancel($recoveryPlanId)
+    {
         return $this->post(
-                'v2/recovery-plans/' . $recoveryPlanId . '/stop',
+                'v2/recovery-plans/' . $recoveryPlanId . '/cancel',
                 json_encode([]),
                 ['Content-Type' => 'application/json']
             )->getStatusCode() === 202;
