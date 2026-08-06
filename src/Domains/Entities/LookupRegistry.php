46eeb5075a913ec661cda1889579cae7f85dfb2b
dawid.hochman <dawid.hochman@ans.co.uk>
2026-08-06 13:09:10 +0200

    Add LookupClient and related entities for domain lookup functionality

---
+++ src/Domains/Entities/LookupRegistry.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace UKFast\SDK\Domains\Entities;
+
+use UKFast\SDK\Entity;
+
+/**
+ * @property string $name
+ * @property string $url
+ * @property string $handle
+ * @property string[] $status
+ */
+class LookupRegistry extends Entity
+{
+}
